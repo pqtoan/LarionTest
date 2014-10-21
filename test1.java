8164844fa47b20ecb31a66901f70fb69e312b11c
unknown <pqtoan@tma.com.vn>
2014-10-21 23:48:50 +0700

    first commit user1 / add display func

--- test1.java
+++ test1.java
@@ -1,5 +1,10 @@
 public class test
 {
+	//this is first commit of user1
+	void display()
+	{
+		System.out.println("hello world");
+	}
 	public static void main(String args[])
 	{
 		System.out.println("Hello this is first commit");
